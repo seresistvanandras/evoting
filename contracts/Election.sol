28d2ae5dd5c2b54575169da17f41b055cf63e40b
seresistvaninterticket <istvan.seres@interticket.net>
2018-02-27 12:23:59 +0100

    Added params for blind sig

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -9,8 +9,13 @@ contract Election is Ownable  {
 
   string public question;
 
-  uint256 public publicModulo;
-  uint256 public publicExponent;
+  //96-bit random prime numbers
+  //prime1 52632226726678600070752309183
+  //prime2 4692554808980366270917812557
+  uint256 public publicModulo = 246979608633620626478852615866591005359742015214903810931;
+  //totient of the modulo 246979608633620626478852615809266223824083048873233689192
+  //privKey: 208099455097861223341963187893673510834579946576437192993
+  uint256 public publicExponent = 65537;
 
   struct Voter {
     bool eligible;
@@ -23,8 +28,8 @@ contract Election is Ownable  {
   //constructor
   function Election (string _question, uint256 _publicModulo, uint256 _publicExponent) public {
     question = _question;
-    publicModulo = _publicModulo;
-    publicExponent = _publicExponent;
+    //publicModulo = _publicModulo;
+    //publicExponent = _publicExponent;
   }
 
 
@@ -32,7 +37,7 @@ contract Election is Ownable  {
     require(eligibleVoters[msg.sender].eligible);
     blindedVotes[blindedVote] = msg.sender;
     RequestToBlindlySign(voter);
-    eligibleVoters[msg.sender].eligible = false; 
+    eligibleVoters[msg.sender].eligible = false;
   }
 
   function writeBlindSig(address _voter, uint256 blindSig) onlyOwner public {
