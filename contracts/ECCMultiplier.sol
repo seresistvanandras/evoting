d7eb11aed67b4e15dd3f15ac29a65e8c1374a266
donfrigo <donfrigo@gmail.com>
2018-03-26 17:04:22 +0200

    added working isOne function

--- contracts/ECCMultiplier.sol
+++ contracts/ECCMultiplier.sol
@@ -28,4 +28,29 @@ library ECCMultiplier {
       return Q;
     }
 
+    function shiftLeft(uint a, uint8 n) public view returns (uint) {
+        return a << n;
+    }
+
+    function isOne(uint a, uint8 n) public view returns (bool) {
+        return convertBytesToBytes32(toBytesEth(a)) & convertBytesToBytes32(toBytesEth(shiftLeft(0x01,n))) != 0;
+    }
+
+    function toBytesEth(uint256 x) public constant returns (bytes) {
+        var b = new bytes(32);
+        for (uint i = 0; i < 32; i++) {
+            b[i] = byte(uint8(x / (2**(8*(31 - i)))));
+        }
+        return b;
+    }
+
+    function convertBytesToBytes32(bytes inBytes) public view returns (bytes32 outBytes32) {
+        if (inBytes.length == 0) {
+            return 0x0;
+        }
+        assembly {
+            outBytes32 := mload(add(inBytes, 32))
+        }
+    }
+
 }
