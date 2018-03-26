b68aeb24dbeedd9233115f087d36a8eb6fa88eb1
seresistvaninterticket <istvan.seres@interticket.net>
2018-03-26 10:35:56 +0200

    Migration files fixed

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,8 +1,5 @@
 var Migrations = artifacts.require("./Migrations.sol");
-var ElectionECC = artifacts.require("./ElectionECC.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(ElectionECC,"What is da question?");
 };
-
--- migrations/3_electionECC_migration.js
+++ migrations/3_electionECC_migration.js
@@ -2,6 +2,8 @@ let ElectionECC = artifacts.require("./ElectionECC.sol");
 let ECCMultiplier = artifacts.require("./ECCMultiplier.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(ECCMultiplier).then( () => {
+    deployer.deploy(ElectionECC, "what is love?");
+  });
     deployer.link(ECCMultiplier, ElectionECC);
-
 };
