a508bd6f3c6f7a74c8c7cee23806dc84939f7361
Janos Gulacsy <donfrigo@gmail.com>
2018-07-23 10:28:46 +0200

    removed package-lock.json and updated package.json

--- contracts/ElectionECCwPrecompile.sol
+++ contracts/ElectionECCwPrecompile.sol
@@ -1,43 +1,41 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.24;
 
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+
+//https://github.com/ethereum/EIPs/pull/213 -> altbn128 precompiles
 import {eccPrecompiles} from './utils/preCompiles.sol';
 
 contract ElectionECCwPrecompile is Ownable {
+
   using SafeMath for uint256;
-  mapping (address => Voter) public eligibleVoters;
-  mapping (uint256 => address) public blindedVotes;
-  mapping (uint256 => uint256) public votes; //counts the votes for specific options
-  mapping (uint256 => bool) public usedSignatures;
+
+  mapping (address => Voter) public eligibleVoters;  // array of eligible voters
+  mapping (uint256 => address) public blindedVotes;  // array of blinded votes
+  mapping (uint256 => uint256) public votes; // counts the votes for specific options
+  mapping (uint256 => bool) public usedSignatures;  // array of used signatures
+
+  event RequestToBlindlySign(address indexed voter);
+  event voteSuccess(address indexed voter, uint256 choiceCode);
 
   string public question;
 
-  //https://github.com/ethereum/EIPs/pull/213
+  uint constant p = 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47; // field modulus
+  uint constant n = 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001; // curve order
 
-  //Y^2 = X^3 + 3
-  uint constant p = 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47; //field modulus
+  // altbn128 generator point
+  uint256[2] public generatorPoint; // in affine coordinates
+  uint256[2] public pubKeyOfOrganizer; // in affine coordinates
 
-  uint constant n = 0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001; //curve order
-
-  // Base point (generator) G
-
-  //altbn128 generator point
-  uint256[2] public generatorPoint; //in affine coordinates
-  uint256[2] public pubKeyOfOrganizer; //in affine coordinates
-
+  // structure that stores voter data
   struct Voter {
     bool eligible;
     uint256 blindedVote;
     uint256 signedBlindedVote;
   }
 
-  event RequestToBlindlySign(address indexed voter);
-  event voteSuccess(address indexed voter, uint256 choiceCode);
-
-
-  //constructor
-  function ElectionECCwPrecompile (string _question) public {
+  // creates election with a predetermined organizer public key, that is used for verification
+  constructor (string _question) public {
     question = _question;
     generatorPoint[0] = 1;
     generatorPoint[1] = 2;
@@ -45,38 +43,43 @@ contract ElectionECCwPrecompile is Ownable {
     pubKeyOfOrganizer[1] = 0x24f4f2f8f95269d63d7cb286934796d9e69ef2b6907d78e8d6ddc6e76cdd748d;
   }
 
-  //blinded message should be recorded in order to be able to verify that Organizer provided a correct signature on the blinded msg
+  // blinded message is recorded in order to verify whether the Organizer has provided a correct signature on the blinded msg
   function requestBlindSig(uint256 blindedVote) public {
     require(eligibleVoters[msg.sender].eligible);
     blindedVotes[blindedVote] = msg.sender;
-    RequestToBlindlySign(msg.sender);
+    emit RequestToBlindlySign(msg.sender);
   }
 
-  //requested blindSig is recorded on the blockchain for auditing purposes
+  // requested blindSig is recorded on the blockchain for auditing purposes
   function writeBlindSig(address _voter, uint256 blindSig) onlyOwner public {
     eligibleVoters[_voter].signedBlindedVote = blindSig;
     eligibleVoters[_voter].eligible = false;
   }
 
-  function Vote(uint256 choiceCode, uint256 c, uint256 s) public {
-    require(!usedSignatures[c]);
-    usedSignatures[c] = true;
-    verifyBlindSig(choiceCode, c, s);
+  // if the blind signature hasn't been used yet and is correct, the vote is valid
+  function Vote(uint256 choiceCode, uint256 blindedVote, uint256 strippedSignedVote) public {
+    require(!usedSignatures[blindedVote]);
+    usedSignatures[blindedVote] = true;
+    verifyBlindSig(choiceCode, blindedVote, strippedSignedVote);
     votes[choiceCode] = votes[choiceCode].add(1);
 
-    voteSuccess(msg.sender,choiceCode);
+    emit voteSuccess(msg.sender,choiceCode);
   }
 
-
+  // organizer can add eligible voters
   function addEligibleVoter(address _voter) onlyOwner public {
+    require(!eligibleVoters[_voter].eligible);
     eligibleVoters[_voter].eligible = true;
   }
 
+  // organizer can remove voters
   function removeEligibleVoter(address _voter) onlyOwner public {
+    require(eligibleVoters[_voter].eligible);
     eligibleVoters[_voter].eligible = false;
   }
 
-  function uintToString(uint v) constant returns (string str) {
+  // converts uint to string
+  function uintToString(uint v) pure public returns (string str) {
     uint maxlength = 78;
     bytes memory reversed = new bytes(maxlength);
     uint i = 0;
@@ -92,16 +95,16 @@ contract ElectionECCwPrecompile is Ownable {
     str = string(s);
   }
 
-  //The core of the contract: onchain ECC BlindSig verification
-  function verifyBlindSig(uint256 m, uint256 c, uint256 s) public {
-    uint[2] memory cP = eccPrecompiles.ecmul(pubKeyOfOrganizer[0], pubKeyOfOrganizer[1], c);
-    uint[2] memory sG = eccPrecompiles.ecmul(generatorPoint[0], generatorPoint[1], s);
+  // the core of the contract: on-chain ECC blind signature verification
+  function verifyBlindSig(uint256 vote, uint256 blindedVote, uint256 strippedSignedVote) public {
+    uint[2] memory cP = eccPrecompiles.ecmul(pubKeyOfOrganizer[0], pubKeyOfOrganizer[1], blindedVote);
+    uint[2] memory sG = eccPrecompiles.ecmul(generatorPoint[0], generatorPoint[1], strippedSignedVote);
 
     uint[2] memory sum = eccPrecompiles.ecadd(cP[0], cP[1], sG[0], sG[1]);
 
     uint projection = sum[0] % n;
 
-    require(c == uint(keccak256(uintToString(m),uintToString(projection))));
+    require(blindedVote == uint(keccak256(uintToString(vote),uintToString(projection))));
   }
 
 }
